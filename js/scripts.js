631168ab79808a9d1bbe3a5b561594001abec60f
jttwnsnd <jt.a.townsend@gmail.com>
2016-08-10 17:31:29 -0400

    Commit 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var citiesApp = angular.module('citiesApp', []);
+var citiesApp = angular.module('citiesApp', ['ngMaterial']);
 citiesApp.controller('citiesController',function($scope, $http){
 
 	var myLatlng = {lat: 40.0000, lng: -98.0000};
@@ -6,8 +6,16 @@ citiesApp.controller('citiesController',function($scope, $http){
 		zoom: 4,
 		center: myLatlng
 	});
+	var reset = document.getElementById('reset')
 	var markers = [];
-
+	google.maps.event.addListener(reset, 'click', function(){
+          	map.setZoom(4);
+          	map.setCenter(myLatlng);
+		});
+	$scope.resetZoom = function(){
+		console.log('hello');
+		google.maps.event.trigger(reset, 'click');
+	}
 	function createMarker(city){
 		var cityLatlng = {lat: city.lat, lng: city.lon};
 		var marker = new google.maps.Marker({
@@ -15,17 +23,24 @@ citiesApp.controller('citiesController',function($scope, $http){
 			map: map,
 			title: city.city
 		});
+		console.log(marker);
 		var infoWindow = new google.maps.InfoWindow({
 			content: city.city
 		});
 
 		google.maps.event.addListener(marker, 'click', function(){
 			infoWindow.open(map, marker);
+          	map.setZoom(8);
+          	map.setCenter(marker.getPosition());
 		})
 		markers.push(marker);
+		$scope.moreInfo = function(){
+			console.log('hello');
+		}
 	}
-	$scope.triggerClick = function(index){
-		google.maps.event.trigger(markers[index], 'click');
+	$scope.triggerClick = function(yearRank){
+		google.maps.event.trigger(markers[yearRank-1], 'click');
+
 	}
 	
 
