24a71db5b9ba23cf49327253d613402904c8ddcd
jttwnsnd <jt.a.townsend@gmail.com>
2016-08-11 15:07:55 -0400

    commit 4
    More Styling. Added more info, get directions, and fixed placement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var citiesApp = angular.module('citiesApp', ['ngMaterial']);
-citiesApp.controller('citiesController',function($scope, $http){
+var citiesApp = angular.module('citiesApp', ['ngMaterial', 'ngMessages']);
+citiesApp.controller('citiesController',function($scope, $http, $mdDialog){
 
 	var myLatlng = {lat: 40.0000, lng: -98.0000};
 	var map = new google.maps.Map(document.getElementById('map'), {
@@ -16,37 +16,86 @@ citiesApp.controller('citiesController',function($scope, $http){
 		console.log('hello');
 		google.maps.event.trigger(reset, 'click');
 	}
+	var infoWindow = new google.maps.InfoWindow({});
 	function createMarker(city){
+		var icon = 'css/pokeball.png';
 		var cityLatlng = {lat: city.lat, lng: city.lon};
 		var marker = new google.maps.Marker({
 			position: cityLatlng,
 			map: map,
-			title: city.city
+			title: city.city,
+			//sets the icon image
+			icon: icon
 		});
-		console.log(marker);
-		var infoWindow = new google.maps.InfoWindow({
-			content: city.city
-		});
-
 		google.maps.event.addListener(marker, 'click', function(){
+			infoWindow.setContent('<h3>' + city.city + '</h3>')
 			infoWindow.open(map, marker);
           	map.setZoom(8);
           	map.setCenter(marker.getPosition());
 		})
+		// var alertContent = "Year Rank: "+ city.yearRank + ", Year Estimate of Population: " + city.yearEstimate + ", Last Census: " + city.lastCensus ", Percent Change: " + city.change + ", Land Area sq mi/km2: " + city.landArea + "/" + city.landAreaInKm + ", Population Density sq mi/km2: " + city.lastPopDensity + "/" + city.lastPopDensityInKM + ", Lat/Lng Location: " + city.latLon;
+		$scope.showAlert = function(ev, city) {
+			var name = city.city +", "+ city.state;
+			var alertContent = 'Year Rank: ' + city.yearRank + ", Year Estimate of Population: " + city.yearEstimate + ", Last Census: " + city.lastCensus + ", Percent Change: " + city.change + ", Land Area sq mi/km2: " + city.landArea + "/" + city.landAreaInKm + ", Population Density sq mi/km2: " + city.lastPopDensity + "/" + city.lastPopDensityInKM + ", Lat/Lng Location: " + city.latLon;
+		// // Appending dialog to document.body to cover sidenav in docs app
+		// // Modal dialogs should fully cover application
+		// // to prevent interaction outside of dialog
+			$mdDialog.show(
+				$mdDialog.alert()
+				.parent(angular.element(document.querySelector('#popupContainer')))
+				.clickOutsideToClose(true)
+				.title(name)
+				.textContent(alertContent)
+				.ariaLabel('Alert Dialog Demo')
+				.ok('Got it!')
+				.targetEvent(ev)
+			);
+		console.log(name);
+		};
 		markers.push(marker);
-		$scope.moreInfo = function(){
-			console.log('hello');
-		}
 	}
 	$scope.triggerClick = function(yearRank){
 		google.maps.event.trigger(markers[yearRank-1], 'click');
 
 	}
-	
+	$scope.$watch('filterCities', function(newValue, oldValue){
+		console.log(newValue);
+	})
 
 	$scope.message = "hello!";
 	$scope.cities = cities;
 	for(var i = 0; i < $scope.cities.length; i++){
 		createMarker($scope.cities[i]);
+	}
+	$scope.updateMarkers = function(){
+		for(var i = 0; i < markers.length; i++){
+			markers[i].setMap(null);
+		}
+		($scope.filteredCities);
+		for(var i = 0; i < $scope.filteredCities.length; i++){
+			createMarker($scope.filteredCities[i]);
+		}
+	}
+
+	$scope.getDirections = function(lat, lon){
+		var changeDestination = new google.maps.LatLng(lat, lon);
+		console.log(changeDestination);
+
+		var directionsService = new google.maps.DirectionsService;
+		var directionsDisplay = new google.maps.DirectionsRenderer;
+		directionsDisplay.setMap(map);
+		directionsDisplay.setPanel(document.getElementById('list-window'));
+		directionsService.route({
+			origin: 'Atlanta, GA',
+			destination: changeDestination,
+			travelMode: 'DRIVING',
+		}, function(response, status){
+			if(status === 'OK'){
+				directionsDisplay.setDirections(response);
+			}else{
+				window.alert('Directions request failed due to ' + status);
+			}
+		})
+		console.log(directionsService.route.destination);
 	}
 });
---
+++ readme.md
@@ -0,0 +1,31 @@
+<h1>City Search</h1>
+<p>A demo of using the google maps API and using Angular, and Angular Materials</p>
+<br />
+<h3>The Goal</h3>
+<p>This is familiarize myself more with working with APIs. Google Maps is a very common one. I wanted the following to be in my basic feature.</p>
+<ul>
+	<li>Place markers at the city locations</li>
+	<li>Have a list displayed for the user to select from</li>
+</ul>
+<p>This gives me a very basic layout and something to build from. The ultimate goal should include the following:</p>
+<ul>
+	<li>A clean layout with wrappers</li>
+	<li>An autocomplete search bar</li>
+	<li>A confined list for the items</li>
+	<li>those items, when clicked, display info and have marker be zoomed in</li>
+</ul>
+<br />
+<h3>Problems and My Solutions</h3>
+<p>I ran into a number of problems and found solutions for these. Some were very simple, other required some thought.</p>
+<ol>
+	<li>Annoying Angular script showing when connection is slow
+		<ul>
+			<li>It would appear when I kept refreshing the page. I researched the docs and discovered ng-cloak. Brilliant and straight-forward.</li>
+		</ul>
+	</li>
+	<li>
+		<ul>
+			<li></li>
+		</ul>
+	</li>
+</ol>
